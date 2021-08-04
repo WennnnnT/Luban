5dd628c776d9894059ca454a3711575716698925
meiliang1123 <meiliang1123@126.com>
2021-08-04 16:13:43 +0800

    Fix: Remove saved environment on recovery canceled

--- src/app/ui/utils/pluginHooksAutoRecovery.js
+++ src/app/ui/utils/pluginHooksAutoRecovery.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
-import { shallowEqual, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import renderRecoveryModal from './renderRecoveryModal';
+
+import { actions as projectActions } from '../../flux/project';
 
 // this is an example of hooks plugin
 export function useRecoveringProject(headType) {
@@ -19,8 +21,15 @@ export function useRecoveringProject(headType) {
     return [recoveringProject, setRecoveringProject];
 }
 export function useRenderRecoveryModal(page) {
+    const dispatch = useDispatch();
     const [recoveringProject, setRecoveringProject] = useRecoveringProject(page);
-    const returnModal = renderRecoveryModal(page, () => { setRecoveringProject(false); });
+    const returnModal = renderRecoveryModal(
+        page,
+        async () => {
+            await dispatch(projectActions.clearSavedEnvironment(page));
+            setRecoveringProject(false);
+        }
+    );
     if (recoveringProject) {
         return returnModal;
     } else {
